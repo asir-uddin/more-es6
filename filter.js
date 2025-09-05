7da92c048caaf22297411ef1fa4e35d288b49ee5
Asir <mdasir017@gmaol.com>
2025-09-05 11:43:43 +0600

    add my filter and find file

--- filter.js
+++ filter.js
@@ -1,4 +1,9 @@
 const number = [ 4, 5, 6, 7, 8, 9, 15, 56];
 
 const greaterThen6 = number.filter(x => x > 6);
-console.log(greaterThen6);
+
+const evenNumber = number.filter(num => num % 2 === 0);
+const oddNumber = number.filter(num => num % 2 === 1);
+console.log(oddNumber)
+// console.log(evenNumber);
+// console.log(greaterThen6);
---
+++ find.js
@@ -0,0 +1,3 @@
+const numbers = [4, 7, 89, 45, 44, 56, 65, 24];
+const findEven = numbers.find(num => num % 2 === 0);
+console.log(findEven);
